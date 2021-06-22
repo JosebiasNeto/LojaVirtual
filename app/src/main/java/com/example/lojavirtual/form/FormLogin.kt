9046ef18d2e331256bc63f8e9ebe78f20f5d892b
Josebias Neto <josebiasbsneto@gmail.com>
2021-06-22 20:32:18 -0300

    Create Login Activity

--- app/src/main/java/com/example/lojavirtual/form/FormLogin.kt
+++ app/src/main/java/com/example/lojavirtual/form/FormLogin.kt
@@ -2,10 +2,12 @@ package com.example.lojavirtual.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lojavirtual.R
 
 class FormLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
+        supportActionBar!!.hide()
     }
 }
