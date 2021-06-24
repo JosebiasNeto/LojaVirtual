7190abcb1899da0ca4a0fd7c966241e73e0b90c4
Josebias Neto <josebiasbsneto@gmail.com>
2021-06-24 06:13:28 -0300

    Create Authentication

--- app/src/main/java/com/example/lojavirtual/form/FormLogin.kt
+++ app/src/main/java/com/example/lojavirtual/form/FormLogin.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lojavirtual.R
+import com.example.lojavirtual.TelaPrincipal
 import com.example.lojavirtual.databinding.ActivityFormLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class FormLogin : AppCompatActivity() {
 
@@ -23,7 +26,42 @@ class FormLogin : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val btEntrar = binding.btEntrar
+        btEntrar.setOnClickListener{
+            AutenticarUsuario()
+        }
 
+
+    }
+    private fun AutenticarUsuario(){
+        val email = binding.etEmail.text.toString()
+        val senha = binding.etSenha.text.toString()
+
+        if (email.isEmpty() || senha.isEmpty()){
+            var snackbar = Snackbar.make(binding.layoutLogin, "Coloque um email e uma senha!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", {
+
+                })
+            snackbar.show()
+        } else {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener {
+                if (it.isSuccessful){
+                    AbrirTelaPrincipal()
+                }
+            }.addOnFailureListener {
+                var snackbar = Snackbar.make(binding.layoutLogin, "Erro ao logar usuário", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("OK", {
+
+                    })
+                snackbar.show()
+            }
+        }
+
+    }
+    private fun AbrirTelaPrincipal(){
+        val intent = Intent(this, TelaPrincipal::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
